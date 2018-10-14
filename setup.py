1141aa43ab5dcd51877e26b877f7e1089b46fb81
Dimiter Naydenov <dimiter@naydenov.net>
2018-10-14 13:57:33 +0300

    Migrated to pipenv

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import find_packages, setup
 
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
 from subreddit_simulator import __version__
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile["packages"], r=False)
 
 setup(
     name="SubredditSimulator",
@@ -12,7 +17,7 @@ setup(
     license="MIT",
     url="https://github.com/dimitern/SubredditSimulator",
     packages=find_packages(),
-    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    install_requires=requirements,
     py_modules=["subreddit_simulator"],
     entry_points="""
     [console_scripts]
